2ec1f43f6576cd1fa033f0d212100955b4f0df20
Igor <ig.vp@hotmail.com>
2021-12-01 10:04:53 -0300

    second challenger solved

---
+++ puzzle2/solution.py
@@ -0,0 +1,16 @@
+
+def solution():
+    f = open("/home/igor/COMP/adventure-code/puzzle2/input.txt", "r")
+    f = f.readlines()
+    f = [int(x[:-1]) for x in f]
+    gratest = 0
+    prev = f[0] + f[1]+ f[2]
+    for i in range(1,len(f)-2):
+        if(f[i]+f[i+1] +f[i+2] > prev):
+            gratest+=1
+        prev = f[i]+f[i+1] +f[i+2]
+    print(gratest)
+    
+
+
+solution()
