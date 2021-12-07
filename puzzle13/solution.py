f23d3cffc16eb59bf19d0acc0b80bec7e2fda63d
Igor <ig.vp@hotmail.com>
2021-12-07 02:22:55 -0300

    13 puzzle done

---
+++ puzzle13/solution.py
@@ -0,0 +1,21 @@
+def solution():
+    f = open("/home/igor/COMP/advent-of-code/puzzle13/input.txt", "r")
+    f = f.readlines()
+    f = "".join(f).split(",")
+    f = [int(x) for x in f]
+    maior = max(f)
+    menor = min(f)
+    cost = float('inf')
+    horizontal = 0
+    for i in range(menor-100,maior+1):
+        custoatual = 0
+        for j in range(len(f)):
+            delta = abs(f[j]-i)
+            custoatual+=delta
+        if(custoatual<cost):
+            cost = custoatual    
+            horizontal = i
+
+    print(cost,horizontal)
+
+solution()
